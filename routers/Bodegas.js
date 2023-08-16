3e816ac651f1c759895a2a2a17bc3abf806fe8c3
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-16 11:02:10 -0500

    feat: :sparkles: implementación del middleware limit/config.js
    Este middleware protege la cantidad de solicitudes que se pueden hacer en un determinado tiempo

--- routers/Bodegas.js
+++ routers/Bodegas.js
@@ -1,5 +1,6 @@
 import { con } from "../db/atlas.js";
 import {Router} from "express";
+import { limitGrt } from "../limit/config.js";
 import { ObjectId } from "mongodb";
 
 const appBodegas =Router();
@@ -7,7 +8,7 @@ const appBodegas =Router();
 let db = await con();
 let Bodegas = db.collection('bodegas');
 
-appBodegas.get("/", async(req, res) => { 
+appBodegas.get("/", limitGrt(), async(req, res) => { 
     let db = await con();
     let Bodegas = db.collection('bodegas');
     let result = await Bodegas.find().toArray();
